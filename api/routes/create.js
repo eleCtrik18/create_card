8253db98e20649efdba33b700f91bce8da97cca0
Chetane <singhchetan0542@gmail.comm>
2021-08-18 14:44:50 +0530

    Final

--- api/routes/create.js
+++ api/routes/create.js
@@ -1,24 +1,21 @@
-const router = require('express').Router();
-const { response } = require('express');
-const User = require('../models/User');
+const router = require("express").Router();
+const { response } = require("express");
+const User = require("../models/User");
 
-router.post("/create", async (req, res) => { 
-    const newUser = new User({
-        username: req.body.username,
-        event: req.body.event,
-        
-    });
-    try {
+router.post("/create", async (req, res) => {
+  const newUser = new User({
+    username: req.body.username,
+    event: req.body.event,
+  });
+  try {
     const user = await newUser.save();
     res.status(201).json(user);
-    } catch (error) {
-        res.status(500).json({
-            message: error.message,
-        });
-    }
-
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
 });
-
 
 
 module.exports = router;
