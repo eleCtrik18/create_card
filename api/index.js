62b0e675feb64de89eb1d60763a9ec7286e73c75
Chetane <singhchetan0542@gmail.comm>
2021-09-03 00:04:22 +0530

    Final

--- api/index.js
+++ api/index.js
@@ -15,9 +15,9 @@ const options = {
 	definition: {
 		openapi: "3.0.0",
 		info: {
-			title: "Library API",
+			title: "Card API",
 			version: "1.0.0",
-			description: "A simple Express Library API",
+			description: "A simple Express Card API",
 		},
 		servers: [
 			{
